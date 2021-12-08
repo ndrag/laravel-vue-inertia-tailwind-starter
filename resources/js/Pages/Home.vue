3f7ded9112386c0ab00f5be5d08b808c15ab2d6c
ndrag <ndraguno@gmail.com>
2021-12-08 15:57:48 +1300

    Add Inertia & Ziggy for routing.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{ exampleProp }}</h1>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  exampleProp: String
+})
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import './bootstrap'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    const app = createApp({ render: () => h(App, props) })
+      .mixin({
+        methods: { route: window.route }
+      })
+      .use(plugin)
+
+    app.mount(el)
+  }
+
+})
